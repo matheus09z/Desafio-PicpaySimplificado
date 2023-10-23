b5fe6869bd2a56dd6dc7a2f3b5366ec05c765f32
matth <mattheew.oliveira@gmail.com>
2023-10-23 10:33:01 -0300

    feat: Creating another constructor

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,11 +1,9 @@
 package com.picpaysimplificado.domain.user;
 
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +12,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of ="id")
 public class User {
 
@@ -32,5 +31,16 @@ public class User {
     private UserType userType;
 
 
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.email = data.email();
+        this.document = data.document();
+
+    }
 
 }
